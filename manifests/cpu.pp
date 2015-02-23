f2b448f3b33a96c207a91c0efdf1ffc10d5dfb1e
Paul Talbot <paul@fullfrontalingenuity.com>
2015-02-23 16:53:34 -0500

    setup cpu

--- manifests/cpu.pp
+++ manifests/cpu.pp
@@ -1,4 +1,8 @@
 # This class checks num of CPUi/vCPU allocated
-class sanity::cpu {
-  # cpu
+class sanity::cpu (
+  $expected_number_cpus = undef,
+) {
+  if $expected_number_cpus and $expected_number_cpus != $::processorcount {
+    err("CPU count! Found ${::processorcount} expected ${expected_number_cpus}")
+  }
 }
