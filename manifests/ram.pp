d919219e344e29a88f784b503e98daf704daaf1e
Paul Talbot <paul@fullfrontalingenuity.com>
2015-02-24 14:24:24 -0500

    typo in ram params

--- manifests/ram.pp
+++ manifests/ram.pp
@@ -1,5 +1,5 @@
 # This class checks allocated RAM
-class sanity::ram {
+class sanity::ram (
   $expected_installed_memory = undef,
 ) {
   if $expected_installed_memory and $expected_installed_memory != $::memorysize_mb {
