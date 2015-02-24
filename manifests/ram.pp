f2adef279b4c0e6673b6219addce90a3e2590b12
Paul Talbot <paul@fullfrontalingenuity.com>
2015-02-24 14:56:34 -0500

    typo in ram.pp

--- manifests/ram.pp
+++ manifests/ram.pp
@@ -1,6 +1,6 @@
 # This class checks allocated RAM
 class sanity::ram (
-  $expected_installed_memory = undef,
+  $expected_installed_memory_mb = undef,
 ) {
   if $expected_installed_memory_mb and $expected_installed_memory_mb != $::memorysize_mb {
     err("RAM size! Found ${::memorysize_mb} expected ${expected_installed_memory_mb}")
