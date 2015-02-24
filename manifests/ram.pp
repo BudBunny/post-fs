16fcf574e81acb20f36fcddc9f450174e6440129
Paul Talbot <paul@fullfrontalingenuity.com>
2015-02-24 14:20:09 -0500

    adding ram.pp to mix

--- manifests/ram.pp
+++ manifests/ram.pp
@@ -1,4 +1,8 @@
 # This class checks allocated RAM
 class sanity::ram {
-  # ram
+  $expected_installed_memory = undef,
+) {
+  if $expected_installed_memory and $expected_installed_memory != $::memorysize_mb {
+    err("RAM size! Found ${::memorysize_mb} expected ${expected_installed_memory}")
+  }
 }
