c1e4fad544c12ebf2fb385bc7a144b6852d8e046
Paul Talbot <paul@fullfrontalingenuity.com>
2015-02-24 14:38:57 -0500

    added notifications on cpu and ram

--- manifests/ram.pp
+++ manifests/ram.pp
@@ -2,6 +2,7 @@
 class sanity::ram (
   $expected_installed_memory = undef,
 ) {
+  notify('sanity::ram')
   if $expected_installed_memory and $expected_installed_memory != $::memorysize_mb {
     err("RAM size! Found ${::memorysize_mb} expected ${expected_installed_memory}")
   }
