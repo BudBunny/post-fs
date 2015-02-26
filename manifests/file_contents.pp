263a84a669ed3126398d40481e5e7ec4086a745a
Paul Talbot <paul@fullfrontalingenuity.com>
2015-02-26 15:15:31 -0500

    added some examples to file_contents.pp

--- manifests/file_contents.pp
+++ manifests/file_contents.pp
@@ -1,8 +1,19 @@
 # This class checks contents of file(s) for string(s)
 class sanity::file_contents {
   # file contents
-  #cat /etc/opt/quest/vas/users.allow
+  #
+  # some examples for /etc/opt/quest/vas/users.allow:
+  #
+  # EDC\P_CIOX_PRD_AppAdmin_FS_Role
+  # EDC\P_OPSX_PRD_AppAdmin_FS_Role
+  # EDC\S_ADDM_UNIX_PRD_Service_NITC_Roles
+  # EDC\S_SCAN_PRD_ServiceAccount_FS_Role
+  # P_Linux_OSAdmin_NITC_Roles
+  # P_Linux_SNCCAdmin_NITC_Role
+  # S_EDC_EnterpriseService_NITC_Roles
+  # S_Hobbit_PRD_Service_NITC_ROLE
+
   file { '/etc/opt/quest/vas/users.allow':
-    ensure => file# Whether the file should exist, and if so what...
+    ensure => file
   }
 }
