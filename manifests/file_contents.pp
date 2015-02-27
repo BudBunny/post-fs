046533477f36dfa2d8fe9ae206a3492d970b601f
Paul Talbot <paul@fullfrontalingenuity.com>
2015-02-26 22:25:58 -0500

    setting up files from hiera_hash for file_contents

--- manifests/file_contents.pp
+++ manifests/file_contents.pp
@@ -25,6 +25,9 @@ class sanity::file_contents {
   #  ensure => file,
   #}
 
+  $files = hiera_array('sanity::file_contents::files', {})
+  create_resources(file, $files)
+
   $file_lines = hiera_hash('sanity::file_contents::file_lines', {})
   create_resources(file_line, $file_lines)
 
