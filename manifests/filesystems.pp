57f3a5348338061ba1017fdc32cdb4b2f6552256
Paul Talbot <paul@fullfrontalingenuity.com>
2015-02-27 14:42:33 -0500

    changed location of external fact

--- manifests/filesystems.pp
+++ manifests/filesystems.pp
@@ -1,4 +1,8 @@
 # This class checks for existance of filesystem(s) and optionally, their size
 class sanity::filesystems {
   # filesystems
+  file { '/etc/facter/facts.d/filesystem_sizes.bash':
+    ensure => file,
+    source => 'puppet:///modules/sanity/filesystem_sizes.bash',
+  }
 }
