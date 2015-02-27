540290b4af5cf6e4d8fab33a5dddc8cf06858e1a
Paul Talbot <paul@fullfrontalingenuity.com>
2015-02-27 14:49:06 -0500

    added dir creation

--- manifests/filesystems.pp
+++ manifests/filesystems.pp
@@ -1,6 +1,11 @@
 # This class checks for existance of filesystem(s) and optionally, their size
 class sanity::filesystems {
   # filesystems
+
+  file { [ '/etc/facter', '/etc/facter/facts.d', ]:
+    ensure => 'directory',
+  }
+
   file { '/etc/facter/facts.d/filesystem_sizes.bash':
     ensure => file,
     source => 'puppet:///modules/sanity/filesystem_sizes.bash',
