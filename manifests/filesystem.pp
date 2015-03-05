d91844b0768ceca433f251c6645e036149d94386
Paul Talbot <paul@fullfrontalingenuity.com>
2015-03-05 14:40:15 -0500

    messing with sasquatch

--- manifests/filesystem.pp
+++ manifests/filesystem.pp
@@ -6,12 +6,7 @@ define sanity::filesystem (
 
   # TODO: add a validate for size here
 
-  if $expected_filesystem_size_bytes > 0 and $expected_filesystem_size_bytes != $::facts[$name] {
-    err("Filesystem! Found ${::facts[$name]} expected ${expected_filesystem_size_bytes}")
+  if $expected_filesystem_size_bytes > 0 and $expected_filesystem_size_bytes != $::facts["sanity_filesystem_${name}"] {
+    err("Filesystem! Found $::facts[sanity_filesystem_${name}] expected ${expected_filesystem_size_bytes}")
   }
-
-  notify {"Inside sanity filesystem ${expected_filesystem_size_bytes}":
-        withpath => true,
-  }
-
 }
