f7279b4566f39843b07944776e42568e9d9fac1f
Bryan Belanger <bbelanger@azcender.com>
2015-03-05 16:03:33 -0500

    Added functionality to filesystem.pp

--- manifests/filesystem.pp
+++ manifests/filesystem.pp
@@ -6,7 +6,14 @@ define sanity::filesystem (
 
   # TODO: add a validate for size here
 
-  if $expected_filesystem_size_bytes > 0 and $expected_filesystem_size_bytes != $::facts["sanity_filesystem_${name}"] {
-    err("Filesystem! Found $::facts[\"sanity_filesystem_${name}\"] expected ${expected_filesystem_size_bytes}")
+  # The mount size reurned from the node
+  $_mount_size = $::facts["sanity_filesystem_${name}"]
+
+  # Check for empty mount point
+  if !$_mount_size {
+    err("The mount point ${name} is missing")
+  }
+  elsif $expected_filesystem_size_bytes > 0 and $expected_filesystem_size_bytes != $_mount_size {
+    err("Filesystem! Found ${_mount_size} expected ${expected_filesystem_size_bytes}")
   }
 }
