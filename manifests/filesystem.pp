80fca7bb41960bb60b30ab8746fed36d9fcddb4b
Paul Talbot <paul@fullfrontalingenuity.com>
2015-03-05 10:42:26 -0500

    fixed variable name to refelect bytes instead of mbytes

--- manifests/filesystem.pp
+++ manifests/filesystem.pp
@@ -1,12 +1,12 @@
 # This class checks for existance of filesystem(s) and optionally, their size
 
 define sanity::filesystem (
-  $expected_filesystem_size_mb = 0,
+  $expected_filesystem_size_bytes = 0,
 ) {
 
   # TODO: add a validate for size here
 
-  if $expected_filesystem_size_mb > 0 and $expected_filesystem_size_mb != $::facts[$name] {
-    err("Filesystem! Found ${::facts[$name]} expected ${expected_filesystem_size_mb}")
+  if $expected_filesystem_size_bytes > 0 and $expected_filesystem_size_bytes != $::facts[$name] {
+    err("Filesystem! Found ${::facts[$name]} expected ${expected_filesystem_size_bytes}")
   }
 }
